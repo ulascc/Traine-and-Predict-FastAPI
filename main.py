9dd7892a83b978c3392a15b008b7acd75ef4d068
ulas.can2424@gmail.com <ulas.can2424@gmail.com>
2023-08-15 18:01:30 +0300

    changed to keep training logs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Query
 from traine import traine_model_async
 from predict import predict
+import datetime
 
 app = FastAPI()
 
@@ -16,11 +17,12 @@ def traine_result(task_id: str):
     result = traine_model_async.AsyncResult(task_id)
     
     if result.state == "SUCCESS":
-        return {"message": "Model trained and saved."}
+        end_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-1]
+        return {"message": "Model trained and saved.", "task_id": task_id, "end_time": end_time, "status" : "complated"}
     elif result.state == "PENDING":
-        return {"message": "Task is still pending. Check back later."}
+        return {"message": "Task is still pending. Check back later.", "task_id": task_id, "status" : "running" }
     else:
-        return {"message": "An error occurred while training the model."}
+        return {"message": "An error occurred while training the model.", "task_id": task_id, "status" : "error"}
 
 
 # Predict
